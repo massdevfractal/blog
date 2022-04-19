85acdf13f650f97edfd677987d6669cc2f5154b4
massdevfractal <massdevfractal@gmail.com>
2022-04-19 19:15:21 +0430

    pagination commit

--- src/main/java/com/nim/blog/controller/PostController.java
+++ src/main/java/com/nim/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.nim.blog.controller;
 
 import com.nim.blog.payload.PostDto;
+import com.nim.blog.payload.PostResponse;
 import com.nim.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,8 +27,13 @@ public class PostController {
 
     // get all posts rest api
     @GetMapping
-    public List<PostDto> getAllPosts(){
-        return postService.getAllPosts();
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     // get post by id
--- src/main/java/com/nim/blog/service/PostService.java
+++ src/main/java/com/nim/blog/service/PostService.java
@@ -1,14 +1,14 @@
 package com.nim.blog.service;
 
 import com.nim.blog.payload.PostDto;
+import com.nim.blog.payload.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    List<PostDto> getAllPosts();
-
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
     PostDto getPostById(long id);
 
     PostDto updatePost(PostDto postDto, long id);
