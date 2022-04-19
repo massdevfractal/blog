7e0fa3c4c97ac33718123238038428ea5813caba
massdevfractal <massdevfractal@gmail.com>
2022-04-19 23:57:04 +0430

    comment part

--- src/main/java/com/nim/blog/entity/Post.java
+++ src/main/java/com/nim/blog/entity/Post.java
@@ -1,15 +1,12 @@
 package com.nim.blog.entity;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -27,4 +24,7 @@ public class Post {
     private String description;
     @Column(name="content", nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 }
