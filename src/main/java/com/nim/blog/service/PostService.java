02f336ac9898b17ccb285c2fe5d34559b8b851fe
massdevfractal <massdevfractal@gmail.com>
2022-04-19 19:06:59 +0430

    first commit

---
+++ src/main/java/com/nim/blog/service/PostService.java
@@ -0,0 +1,17 @@
+package com.nim.blog.service;
+
+import com.nim.blog.payload.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
